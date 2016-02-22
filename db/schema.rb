6fe455039d92b6f3d71c5a483156f4e7a9ca1030
Mark O'Russa <mark@markproaudio.com>
2016-02-22 08:46:32 -0800

    Merging 'profile'

--- db/schema.rb
+++ db/schema.rb
@@ -14,62 +14,62 @@
 ActiveRecord::Schema.define(version: 20160221183215) do
 
   create_table "friends", force: true do |t|
-    t.string "user_id"
-    t.string "friend_id"
-    t.boolean "friends"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text    "user_id"
+    t.text    "friend_id"
+    t.integer "friends"
+    t.text    "created_at"
+    t.text    "updated_at"
   end
 
   create_table "galaxies", force: true do |t|
-    t.integer "author", limit: 255
-    t.text "content"
-    t.string "image"
-    t.string "tag"
+    t.integer  "author",     limit: 255
+    t.text     "content"
+    t.string   "image"
+    t.string   "tag"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "privacy_id"
+    t.integer  "privacy_id"
   end
 
   create_table "privacy_levels", force: true do |t|
-    t.string "display"
-    t.string "description"
+    t.string   "display"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "profiles", force: true do |t|
-    t.string "name"
-    t.integer "age"
+    t.string   "name"
+    t.integer  "age"
     t.datetime "dob"
-    t.string "email"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.string "reset_password_sent_at"
-    t.string "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "fName"
-    t.string "lName"
-    t.integer "age"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "fName"
+    t.string   "lName"
+    t.integer  "age"
     t.datetime "dob"
-    t.string "avatar"
-    t.string "comments"
+    t.string   "avatar"
+    t.string   "comments"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
