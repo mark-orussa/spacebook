7cfc23b21dd412c1367d50be339159bfb3a2bc96
Jon Pool <reddevpool@gmail.com>
2016-02-23 06:47:04 -0800

    remove carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,6 @@
 ActiveRecord::Schema.define(version: 20160221183215) do
 
   create_table "friends", force: true do |t|
-    t.text    "user_id"
-    t.text    "friend_id"
-    t.integer "friends"
-    t.text    "created_at"
-    t.text    "updated_at"
-  end
-
-  create_table "friends_old", force: true do |t|
     t.string   "user_id"
     t.string   "friend_id"
     t.boolean  "friends"
